ce1e6141d7bc63b9bb6582a16ff65f493feb6371
HabibaNezar <habibanezarr.com>
2025-07-15 08:10:21 +0300

    component name modified

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import style from './style.module.css';
+import style from './Style.module.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
